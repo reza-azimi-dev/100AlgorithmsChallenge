1d0493f57ffcb06899210eb431884d684179e25c
j3l4ck0ut <r.enigma1995@gmail.com>
2020-07-19 22:39:22 +0200

    1  python equivalent

---
+++ absoluteValuesSumMinization/absoluteValuesSumMinimization.py
@@ -0,0 +1,9 @@
+def absoluteValuesSumMinimization(number):
+    isEven = len(number) % 2 == 0
+    if isEven 
+    return
+
+print(absoluteValuesSumMinimization([2, 4, 7]))
+print(absoluteValuesSumMinimization([2, 4, 7, 6]))
+print(absoluteValuesSumMinimization([2, 4, 7, 6, 6]))
+print(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]))
