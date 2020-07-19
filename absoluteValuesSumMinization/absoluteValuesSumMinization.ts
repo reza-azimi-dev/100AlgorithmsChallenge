24276868cf4100405c002127c89c5a7330fdff1e
j3l4ck0ut <r.enigma1995@gmail.com>
2020-07-19 22:24:33 +0200

    testing

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -6,3 +6,4 @@ function absoluteValuesSumMinimization(a: number[]): number {
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+// test
