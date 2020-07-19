0c31b74930c1ef00460bcb16f318f356f6983a59
j3l4ck0ut <r.enigma1995@gmail.com>
2020-07-19 22:27:12 +0200

    deleting test in the file

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -6,4 +6,3 @@ function absoluteValuesSumMinimization(a: number[]): number {
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
-// test
