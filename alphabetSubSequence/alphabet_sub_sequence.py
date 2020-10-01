e7c8af8d3857f0e3ad3c6f88d9759bf3349f80c6
j3l4ck0ut <r.enigma1995@gmail.com>
2020-10-01 14:28:53 +0200

    solved in python

--- alphabetSubSequence/alphabet_sub_sequence.py
+++ alphabetSubSequence/alphabet_sub_sequence.py
@@ -1,1 +1,18 @@
-#
+def alphabet_sub_sequence(s): 
+
+    char_values = [ord(i) for i in list(s)]
+    char_values_set = set(char_values)
+
+    if len(char_values) != len(char_values_set):
+        return False
+    for i in range(len(char_values)-1):
+        if char_values[i] >= char_values[i+1]:
+            
+            return False
+    return True
+    
+print(alphabet_sub_sequence('zab'))
+print(alphabet_sub_sequence('effg'))
+print(alphabet_sub_sequence('cdce'))
+print(alphabet_sub_sequence('ace'))
+print(alphabet_sub_sequence('bxz'))
