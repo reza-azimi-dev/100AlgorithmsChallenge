fae4c3baca2c85f9f800bf4a74c031971940e827
Reza Azimi <59685083+reza-azimi-dev@users.noreply.github.com>
2021-07-23 11:41:29 +0200

    addborder answer

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,14 @@
 function addBorder(picture: string[]): string[] {
-
+    const lenChar = picture[1].length
+    
+    let topBottomBorder = '*'.repeat(lenChar + 2);
+    
+    const content = picture.map(el => {return `*${el}*`})
+    
+    picture.map((el,idx )=> { picture[idx] = `*${el}*`});
+    picture.unshift(topBottomBorder)
+    picture.push(topBottomBorder)
+    return picture
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded", "cdc"]));
